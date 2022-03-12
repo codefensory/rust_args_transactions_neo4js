b3893690d15154ae200ebebb43a846a3d850075a
Yeferson <yefer.el.teke@gmail.com>
2022-03-11 23:02:28 -0500

    Add Outputs and Inputs to transactions

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,9 +1,26 @@
 use base58::ToBase58;
 use chrono::Utc;
 use neo4rs::Graph;
-use uuid::Uuid;
 use serde::Serialize;
-use sha2::{Sha256, Digest};
+use sha2::{Digest, Sha256};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize)]
+pub struct Output {
+   id: u32,
+   value: f64,
+   address: String,
+}
+
+pub struct Input {
+   prev_tx: String,
+   id: u32,
+   value: f64,
+
+   address: String,
+   public_key: String,
+   signature: String,
+}
 
 #[derive(Debug, Serialize)]
 pub struct Transaction {
@@ -12,6 +29,9 @@ pub struct Transaction {
    date: i64,
    vin_hash: String,
    vout_hash: String,
+
+   #[serde(skip_serializing)]
+   vout: Vec<Output>,
 }
 
 impl Transaction {
@@ -22,19 +42,39 @@ impl Transaction {
          date: Utc::now().timestamp(),
          vin_hash: String::new(),
          vout_hash: String::new(),
+         vout: Vec::new(),
       }
    }
 
    pub fn generate_hash(&mut self) {
+      let vout_json = serde_json::to_string(&self.vout).unwrap();
+
+      let mut vout_hasher = Sha256::new();
+      vout_hasher.update(&vout_json);
+      self.vout_hash = hex::encode(vout_hasher.finalize());
+
+      let mut self_hasher = Sha256::new();
       let self_json = serde_json::to_string(&self).unwrap();
-      let mut hasher = Sha256::new();
-      hasher.update(&self_json);
-      self.hash = hex::encode(hasher.finalize());
+      self_hasher.update(&self_json);
+      self.hash = hex::encode(self_hasher.finalize());
+   }
+
+   pub fn add_output(&mut self, amount: f64, to_address: String) {
+      let output = Output {
+         id: self.vout.len() as u32,
+         value: amount,
+         address: to_address,
+      };
+
+      self.vout.push(output);
    }
 }
 
-pub fn create_coinbase(_to_address: String, _amount: String, _graph: Graph) {
+pub fn create_coinbase(to_address: String, amount: String, _graph: Graph) {
    let mut transaction = Transaction::new();
+
+   transaction.add_output(amount.parse::<f64>().unwrap(), to_address);
+
    transaction.generate_hash();
    println!("{:?}", transaction)
 }
