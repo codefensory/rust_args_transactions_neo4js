590fa8c4881a91d1c6e3bb441bdd1cbee219f63f
Yeferson <yefer.el.teke@gmail.com>
2022-03-16 16:25:45 -0500

    Delete public_key from key_pairs

--- src/key_pair.rs
+++ src/key_pair.rs
@@ -7,7 +7,6 @@ pub fn generate_keys() {
     let public_key = private_key.public_key().to_encoded_point(false);
 
     println!("Private Key: {}", hex::encode(private_key.to_be_bytes()));
-    println!("Public Key: {}", hex::encode(&public_key));
 
     let mut hasher = Ripemd160::new();
     hasher.update(&public_key);
